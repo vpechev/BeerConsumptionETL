069021af8ef6db0b524551c44a63e797e807a550
Vladimir Pechev <vladimir.pechev@scalefocus.com>
2019-06-18 19:39:41 +0300

    Implement config-server and configuration-repository

---
+++ config-server/src/test/java/com/scalefocus/config/server/ConfigServerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.scalefocus.config.server;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerApplicationTest {
+    @Test
+    public void applicationStartTest(){
+
+    }
+}
